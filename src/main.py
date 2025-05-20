49879f9aaa1d28abb2c8dca1df587e154d1c679c
Salomon Metre <kulosmetros088@gmail.com>
2025-05-20 12:31:59 +0300

    modify main file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
 from datetime import datetime
 from fastapi import FastAPI
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
-    date: datetime = None
+    date: datetime = Field(default_factory=datetime.utcnow)  # set current time by default
 
 @app.get('/hello')
 async def say_hello():
-    return {'message': 'Hello, World!'}
+    return {"message": "Hello, World!"}
 
 @app.post('/post')
 async def post_data(post: Post):
     return post
-
